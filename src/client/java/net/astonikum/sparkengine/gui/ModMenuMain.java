8fd61c656dfd92a99dc0a7d8988aca38547edf77
Astonikum <astonikum@gmail.com>
2024-06-23 16:12:39 +0300

    builded 0.0.4

--- src/client/java/net/astonikum/sparkengine/gui/ModMenuMain.java
+++ src/client/java/net/astonikum/sparkengine/gui/ModMenuMain.java
@@ -5,39 +5,11 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.MutableText;
 
 public class ModMenuMain extends Screen {
+    private final Screen parent;
+
     public ModMenuMain(Screen parent, MutableText translatable) {
         super(translatable);
+        this.parent = parent;
     }
-
-  /*protected TutorialScreen() {
-    // The parameter is the title of the screen,
-    // which will be narrated when you enter the screen.
-    super(Text.literal("Spark Engine Config"));
-  }
-
-
-  public ButtonWidget button1;
-  public ButtonWidget button2;
-
-  @Override
-  protected void init() {
-
-    button1 = ButtonWidget.builder(Text.literal("Button 1"), button -> {
-      System.out.println("You clicked button1!");
-    })
-        .dimensions(width / 2 - 205, 20, 200, 20)
-        .tooltip(Tooltip.of(Text.literal("Tooltip of button1")))
-        .build();
-    button2 = ButtonWidget.builder(Text.literal("Button 2"), button -> {
-      System.out.println("You clicked button2!");
-    })
-        .dimensions(width / 2 + 5, 20, 200, 20)
-        .tooltip(Tooltip.of(Text.literal("Tooltip of button2")))
-        .build();
-
-    addDrawableChild(button1);
-    addDrawableChild(button2);
-
-  }*/
 
 }
--- src/client/java/net/astonikum/sparkengine/mixin/client/OptionsScreenMixin.java
+++ src/client/java/net/astonikum/sparkengine/mixin/client/OptionsScreenMixin.java
@@ -1,11 +1,13 @@
 package net.astonikum.sparkengine.mixin.client;
 
 import net.astonikum.sparkengine.gui.ModMenuMain;
+import net.astonikum.sparkengine.gui.core.IconButtonWidget;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,15 +16,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(OptionsScreen.class)
 public abstract class OptionsScreenMixin extends Screen {
 
+    private static final Identifier ICON_TEXTURE = new Identifier("spark_engine", "/assets/gui/settings-logo.png");
+
     protected OptionsScreenMixin(Text title) {
         super(title);
     }
 
     @Inject(at = @At("RETURN"), method = "init")
     public void init(CallbackInfo ci) {
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 185, this.height / 6 + 120 - 6, 20, 20, Text.translatable("options.resourcepack"), (button) -> {
-            MinecraftClient.getInstance().setScreen(new ModMenuMain(this, Text.translatable("resourcePack.title")));
-        }));
+        this.addDrawableChild(new IconButtonWidget(this.width / 2 - 180, this.height / 6 + 120 - 6, 20, 20, (button) -> {
+            MinecraftClient.getInstance().setScreen(new ModMenuMain(this, Text.translatable("spark_engine.mod_settings.hub.title")));
+        }, ICON_TEXTURE));
     }
 
 }
