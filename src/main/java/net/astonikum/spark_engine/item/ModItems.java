297b21bd39c0595e3737018fdb387cbfb52a7b00
Astonikum <astonikum@gmail.com>
2024-06-26 01:38:42 +0300

    builded 0.0.7 (test) AND GITIGNORE UPD

--- src/main/java/net/astonikum/spark_engine/item/ModItems.java
+++ src/main/java/net/astonikum/spark_engine/item/ModItems.java
@@ -1,12 +1,19 @@
 package net.astonikum.spark_engine.item;
 
+import net.astonikum.spark_engine.SparkEngine;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-//    public static final Item ACTIVE_SCRIPTS_MONITOR = registerItem(Text.translatable("spark_engine.item.active_scripts_monitor" ),
-//            new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item ACTIVE_SCRIPTS_MONITOR = registerItem(String.valueOf(Text.translatable("item.spark_engine.active_scripts_monitor" )),
+            new Item(new FabricItemSettings().maxCount(1)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(SparkEngine.MOD_ID, name), item);
+    }
 
     public static void registerModItems() {
 
