53bb47de3208f9334097ba08efc2c5cb03af693d
Astonikum <astonikum@gmail.com>
2024-07-05 19:18:55 +0300

    working on Script Editor 4

--- src/client/java/net/astonikum/spark_engine/gui/CodeEditor.java
+++ src/client/java/net/astonikum/spark_engine/gui/CodeEditor.java
@@ -1,23 +1,28 @@
 package net.astonikum.spark_engine.gui;
 
+import net.minecraft.client.font.FontStorage;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.EditBox;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class CodeEditor extends Screen {
-    private final String scriptName;
-    private final Screen parent;
+    private String scriptName;
+    private Screen parent;
 
-    protected CodeEditor(String title, Screen parent, String scriptName) {
+    public CodeEditor(String title, Screen parent, String scriptName) {
         super(Text.of(title + " - " + scriptName));
         this.parent = parent;
         this.scriptName = scriptName;
     }
+
+    EditBox editBox = new EditBox(new TextRenderer(new Function< Identifier, FontStorage >), this.width);
 
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
